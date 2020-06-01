530682da51792f9cb2ac5718d3fafdee55d26625
janthenelli <57114460+janthenelli@users.noreply.github.com>
2020-06-01 19:43:36 -0400

    unzipped project folder

---
+++ HailCaesar/HailCaesar/src/edu/mccc/cos210/hailcaesar/RN_Calculator.java
@@ -0,0 +1,13 @@
+package edu.mccc.cos210.hailcaesar;
+
+import java.awt.EventQueue;
+
+import edu.mccc.cos210.hailcaesar.calc.RN_Calc;
+import edu.mccc.cos210.hailcaesar.ui.RN_Calc_UI;
+
+public class RN_Calculator {
+	public static void main(String[] args) {
+		RN_Calc rnc = new RN_Calc();
+		EventQueue.invokeLater(() -> new RN_Calc_UI(rnc));
+	}
+}
